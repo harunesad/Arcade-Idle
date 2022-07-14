7f1af439f3b278edc72b3e4bff3c7eb0a67dc912
Harun Aşık <harun.esad@gmail.com>
2022-07-14 17:08:07 +0300

    commit

--- Assets/Scripts/NewStack.cs
+++ Assets/Scripts/NewStack.cs
@@ -14,11 +14,12 @@ public class NewStack : MonoBehaviour
     float distanceToStack;
     float distanceToStackCollision;
 
-    public float posZ;
+    float posZ;
 
     int stepOne = 10;
     int stepTwo = 15;
-    int stepThree = 25;
+    int stepThree = 23;
+    int stepFour = 28;
 
     public bool isMove = false;
     private void Awake()
@@ -33,11 +34,11 @@ public class NewStack : MonoBehaviour
     {
         distanceToStack = Vector3.Distance(player.transform.position, transform.position);
         //Debug.Log(distanceToStack);
-        if (distanceToStack < 2)
+        if (StackControl.instance.stackObjects.Count > 0)
         {
-            if (StackControl.instance.stackObjects.Count > 0)
+            Transform stack = StackControl.instance.stackObjects[StackControl.instance.stackObjects.Count - 1].transform;
+            if (distanceToStack < 2)
             {
-                Transform stack = StackControl.instance.stackObjects[StackControl.instance.stackObjects.Count - 1].transform;
                 stack.transform.parent = parent.transform;
                 distanceToStackCollision = Vector3.Distance(stack.localPosition, stackPointBuild.transform.localPosition);
                 stack.position = Vector3.Lerp(stack.position, stackPointBuild.transform.position, Time.deltaTime * 3);
@@ -56,7 +57,7 @@ public class NewStack : MonoBehaviour
                     StackControl.instance.stackObjects[StackControl.instance.stackObjects.Count - 1].transform.rotation = Quaternion.Euler(0, 90, 0);
                 }
 
-                else if (parent.transform.childCount > stepThree)
+                else if (parent.transform.childCount >= stepThree && parent.transform.childCount < stepFour)
                 {
                     StackControl.instance.stackObjects[StackControl.instance.stackObjects.Count - 1].transform.rotation = Quaternion.Euler(0, 0, 0);
                 }
@@ -101,11 +102,30 @@ public class NewStack : MonoBehaviour
                         }
                         else if (parent.transform.childCount == stepThree - 1)
                         {
-                            stackPointBuild.transform.position = new Vector3(stackPointBuild.transform.position.x, stackPointBuild.transform.position.y, stackPointBuild.transform.position.z);
+                            stackPointBuild.transform.position = new Vector3(stackPointBuild.transform.position.x - 0.15f, stackPointBuild.transform.position.y, stackPointBuild.transform.position.z - 0.26f);
+                        }
+                    }
+                    else if (parent.transform.childCount >= stepThree && parent.transform.childCount < stepFour)
+                    {
+                        if (parent.transform.childCount < stepFour - 1)
+                        {
+                            stackPointBuild.transform.position = new Vector3(stackPointBuild.transform.position.x - 0.41f, stackPointBuild.transform.position.y, stackPointBuild.transform.position.z);
+                        }
+                        else if (parent.transform.childCount == stepFour - 1)
+                        {
+                            stepOne += 26;
+                            stepTwo += 26;
+                            stepThree += 26;
+                            stepFour += 26;
+                            stackPointBuild.transform.position = new Vector3(stackPointBuild.transform.position.x - 0.15f, stackPointBuild.transform.position.y + 0.11f, stackPointBuild.transform.position.z + 0.26f);
                         }
                     }
                     playerStack.transform.position = new Vector3(playerStack.transform.position.x, playerStack.transform.position.y - 0.15f, playerStack.transform.position.z);
                 }
+            }
+            if (stack.transform.parent == parent.transform)
+            {
+                stack.position = Vector3.Lerp(stack.position, stackPointBuild.transform.position, Time.deltaTime * 3);
             }
         }
         Debug.Log(distanceToStackCollision);
