2fbfd37477c0fa7325ee12c371fdf7d1f5b8bf9d
Harun Aşık <harun.esad@gmail.com>
2022-07-28 11:40:16 +0300

    commit

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -12,10 +12,11 @@ public class Target : MonoBehaviour
     float newPosx, newPosy, newPosz;
     float posX, posY, posZ;
     bool archerMove = false;
-    int index = 0;
+    public int index = 0;
     int archerIndex;
     float distanceArcherMove;
     float stopArcherMove = 0.1f;
+    int step = 5;
     private void Awake()
     {
         instance = this;
@@ -47,6 +48,7 @@ public class Target : MonoBehaviour
                         posZ = newPosz - oldPosZ;
                         Debug.Log(hit.point);
                         transform.position = new Vector3(newPosx, newPosy, newPosz);
+                        index = 0;
                         archerMove = true;
                         //ArcherBuild.instance.archers[index].GetComponent<NavMeshAgent>().isStopped = false;
 
@@ -56,6 +58,10 @@ public class Target : MonoBehaviour
                         //    archersPos.position = new Vector3(archersPos.transform.position.x + posX, archersPos.transform.position.y, archersPos.transform.position.z + posZ);
                         //}
                         //archerBuild.activeSelf
+                        if (stopArcherMove > 0.5f)
+                        {
+                            stopArcherMove--;
+                        }
                     }
                     playerClick = false; 
                 }
@@ -78,13 +84,31 @@ public class Target : MonoBehaviour
             ArcherBuild.instance.archers[index].GetComponent<Animator>().SetFloat("Run", 2);
             ArcherBuild.instance.archers[index].GetComponent<NavMeshAgent>().destination = archersPos.position + new Vector3(posX, 0, posZ);
 
+            if (ArcherBuild.instance.archers.Count >= step && ArcherBuild.instance.archers.Count < step + 5)
+            {
+                stopArcherMove++;
+                step += 5;
+            }
             distanceArcherMove = ArcherBuild.instance.archers[index].transform.position.z - transform.position.z;
+            if (index < ArcherBuild.instance.archers.Count - 1 && Mathf.Abs(distanceArcherMove) >= stopArcherMove)
+            {
+                index++;
+            }
             if (Mathf.Abs(distanceArcherMove) < stopArcherMove)
             {
+                if (index == ArcherBuild.instance.archers.Count - 1)
+                {
+                    index = 0;
+                    //stopArcherMove--;
+                }
                 ArcherBuild.instance.archers[index].GetComponent<NavMeshAgent>().isStopped = true;
                 ArcherBuild.instance.archers[index].GetComponent<Animator>().SetFloat("Run", 1);
                 index++;
             }
+            //if (index == ArcherBuild.instance.archers.Count - 1)
+            //{
+            //    index = 0;
+            //}
             Debug.Log(distanceArcherMove);
         }
     }
